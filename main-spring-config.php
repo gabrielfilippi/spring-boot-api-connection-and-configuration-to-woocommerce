10af56fadbaefc6c846a6a6e123be2447dcaa477
Gabriel Filippi <38786798+GabrielFilippi@users.noreply.github.com>
2022-10-20 14:26:43 -0300

    added hook to change order status in woocommerce API

--- main-spring-config.php
+++ main-spring-config.php
@@ -5,7 +5,7 @@
  * Description: This plugin connects woocommerce with Floricultura Filippi's custom system using Spring Boot.
  * Author: Gabriel Filippi
  * Author URI: #
- * Version: 0.2.0
+ * Version: 0.2.1
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
@@ -19,6 +19,7 @@ class MainSpringConnection{
     public function __construct() {
         // This hook will run when the plugin is activated and call the activate function
         register_activation_hook(__FILE__, '__spring_boot_API_controll_db');
+        add_filter('woocommerce_reports_order_statuses', [$this, 'set_order_statuses_in_sales_reports']);
     }
 
     /**
@@ -40,6 +41,15 @@ class MainSpringConnection{
     
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
         dbDelta( $sql );
+    }
+
+    /**
+    * Set order status to get sales reports FROM API.
+    * 
+    * @since 20/10/2022
+    */
+    function set_order_statuses_in_sales_reports( $statuses ){
+        return array( 'processing', 'completed' );
     }
 
 }
